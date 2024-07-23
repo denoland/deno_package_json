2abe83806720ab8163ef7c22fbad3ac78bc00706
David Sherret <dsherret@gmail.com>
2024-07-23 13:34:38 -0400

    format

--- src/fs.rs
+++ src/fs.rs
@@ -4,10 +4,8 @@ use std::borrow::Cow;
 use std::path::Path;
 
 pub trait DenoPkgJsonFs {
-  fn read_to_string_lossy(
-    &self,
-    path: &Path,
-  ) -> Result<String, std::io::Error>;
+  fn read_to_string_lossy(&self, path: &Path)
+    -> Result<String, std::io::Error>;
 }
 
 impl<'a> Default for &'a dyn DenoPkgJsonFs {
